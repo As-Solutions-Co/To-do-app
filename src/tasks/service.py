251fddb8f3cf1a8a44fc7dfeeadcef14e85d7a86
Juan Diego <juandiegar@gmail.com>
2025-08-02 22:53:09 -0500

    create new branch for development, learning Auth on FastAPI

--- src/tasks/service.py
+++ src/tasks/service.py
@@ -12,3 +12,13 @@ def get_tasks():
 @tasks_router.post("/create_task")
 def create_task():
     pass
+
+
+@tasks_router.put("/update_task")
+def update_task():
+    pass
+
+
+@tasks_router.delete("/delete_task")
+def delete_task():
+    pass
